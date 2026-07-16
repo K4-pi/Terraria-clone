a7e52865dab8e8c33c089c5f82176e4d11f3cfcc
K4-pi <Kap1_priv@proton.me>
2026-07-16 18:26:31 +0200

    files relocation

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     }
     SDL_SetRenderLogicalPresentation(renderer, GameContext::BASE_RESOLUTION.x, GameContext::BASE_RESOLUTION.y, SDL_LOGICAL_PRESENTATION_OVERSCAN);
 
-    if (LoadTexturesFromFile(renderer, "tilemap.png") == -1)
+    if (LoadTexturesFromFile(renderer, "resources/tilemap.png") == -1)
     {
         SDL_Log("Failed to load textures from a file: %s", SDL_GetError());
         return SDL_APP_FAILURE;
